806f7ad1701e4ab46f0531b2069292ea8c1afac7
ManjiriNaik <114968931+ManjiriNaik@users.noreply.github.com>
2023-10-30 00:23:01 +0530

    Add files via upload

---
+++ music-list2.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "Ae Watan Watan",
+      artist: "Arijit Singh , Sunidhi Chauhan",
+      img: "AeWatan",
+      src: "AeWatan"
+    },
+    {
+      name: "Nahi Kalale Kadhi",
+      artist: "Mangesh Borgaonkar",
+      img: "NahiKalaleKadhi",
+      src: "NahiKalaleKadhi"
+    },
+    {
+      name: "Tula Pahta",
+      artist: "Hrishikesh Ranade",
+      img: "TulaPahta",
+      src: "TulaPahta"
+    },
+    {
+      name: "Hardwind - Want Me",
+      artist: "Mike Archangelo",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Jaanta Ajaanta",
+      artist: "Shaan ",
+      img: "JaantaAjaanta",
+      src: "JaantaAjaanta"
+    },
+    {
+      name: "Lost Sky - Vision NCS",
+      artist: "NCS Release",
+      img: "music-6",
+      src: "music-6"
+    },
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
